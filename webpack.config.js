7a7dc25e95975edc5460c5e0053c4bc349c0b2ef
itechdom <oalghnmi@gmail.com>
2019-10-16 14:55:31 -0400

    removing build for now

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,7 @@ module.exports = {
   output: {
     path: path.join(__dirname, "lib"),
     chunkFilename: "[name].bundle.js",
-    filename: "[name]",
+    filename: "[name].js",
     libraryTarget: "umd",
     umdNamedDefine: true
   },
